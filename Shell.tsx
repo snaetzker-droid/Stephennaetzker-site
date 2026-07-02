0ccd7176a49d6644eb32ec629bd9a20746653b8c
snaetzker-droid <snaetzker@gmail.com>
2026-07-02 19:12:27 -0400

    Update Shell.tsx

--- Shell.tsx
+++ Shell.tsx
@@ -1,2 +1,5 @@
-import { PageShell } from '../../components/layout/Shell';
-export default function Resume(){return <PageShell title="Resume"><p>Teaching, sales, leadership, communication, military service, and evidence-based problem solving.</p></PageShell>}
+import { PageShell } from './components/layout/Shell';
+
+export default function Shell() {
+  return <PageShell title="Resume"><p>Teaching, sales, leadership, communication, military service, and evidence-based problem solving.</p></PageShell>;
+}
